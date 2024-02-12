32f3a87879e527fdf17f006d89805a0203d4d2d2
imac <busuko@yahoo.com>
2024-02-12 13:44:43 +0200

    2

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs
+from dotenv import load_dotenv
+import os
+
 from sqlalchemy.orm import DeclarativeBase
-from config import settings
 
-engine = create_async_engine(str(settings.DATABASE_URL))
+load_dotenv()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
--- migrations/env.py
+++ migrations/env.py
@@ -1,53 +1,29 @@
 from logging.config import fileConfig
-import sys
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from os.path import abspath, dirname
+
 from alembic import context
 from sqlalchemy.ext.asyncio import AsyncEngine
+from dotenv import load_dotenv
+import os
 
-from config import settings
-from hotels.models import Hotels, Rooms
-from booking.models import Bookings
-from users.models import Users
-from database import Base
+load_dotenv()
 
-# sys.path.insert(0, dirname(dirname(abspath(__file__))))
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
-# config.set_main_option('sqlalchemy.url', f'{settings.DATABASE_URL}?async_fallback=True')
+from hotels.models import Base as HotelBase
+from booking.models import Base as BookingBase
+from users.models import Base as UserBase
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# Set up logging configuration
+if context.config.config_file_name:
+    fileConfig(context.config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# Define target metadata
+target_metadata = [HotelBase.metadata, BookingBase.metadata, UserBase.metadata]
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
+# Define function to run migrations offline
+def run_migrations_offline():
+    url = context.config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -59,11 +35,12 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+# Define function to run migrations online
 def run_migrations_online():
     connectable = AsyncEngine(
         engine_from_config(
-            config.get_section(config.config_ini_section),
-            prefix='sqlalchemy.',
+            context.config.get_section(context.config.config_ini_section),
+            prefix="sqlalchemy.",
             poolclass=pool.NullPool,
         )
     )
@@ -78,4 +55,8 @@ def run_migrations_online():
             context.run_migrations()
 
 
-run_migrations_online()
+# Choose whether to run migrations offline or online based on configuration
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
