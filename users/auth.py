999ebd1978965dcca3543b9138fb4f62f246cc31
imac <busuko@yahoo.com>
2024-05-09 15:51:06 +0300

    add login

--- users/auth.py
+++ users/auth.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+from pydantic import EmailStr
+import jwt
+from users.dao import UsersDAO
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -9,3 +13,20 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode, 'dshrsfhbs', 'HS256'
+    )
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
--- users/router.py
+++ users/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
+from starlette.responses import Response
 
-from users.auth import get_password_hash
+from users.auth import get_password_hash, authenticate_user, create_access_token
 from users.dao import UsersDAO
-from users.schemas import SUserRegister
+from users.schemas import SUserAuth
 
 router = APIRouter(
     prefix='/auth',
@@ -11,10 +12,19 @@ router = APIRouter(
 
 
 @router.post('/register')
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
 
+
+@router.post('/login')
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({'sub': user.id})
+    response.set_cookie('booking_access_token', access_token, httponly=True)
+    return access_token
