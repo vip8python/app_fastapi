8eca2570ad364280ea28ed0426c40bf76fbe7580
imac <busuko@yahoo.com>
2024-05-11 13:30:20 +0300

    add redis and celery

--- tasks/celery_tasks.py
+++ tasks/celery_tasks.py
@@ -10,7 +10,7 @@ def process_pic(
     im_path = Path(path)
     im = Image.open(im_path)
     im_resized_1000_500 = im.resize((1000, 500))
-    im_resized_200_100 = im.resize((200, 100))
+    im_resized_200_100 = im.resize((300, 100))
     im_resized_1000_500.save(f'static/images/resized_1000_500_{im_path.name}')
     im_resized_200_100.save(f'static/images/resized_200_100_{im_path.name}')
 
